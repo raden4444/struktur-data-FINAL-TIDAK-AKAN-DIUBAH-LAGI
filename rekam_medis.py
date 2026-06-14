c82eb96f1d57ae9561df434a25a2a72c485179a2
raden4444 <radenfadlanalfani@gmail.com>
2026-06-14 22:33:55 +0700

    Update rekam_medis.py

--- rekam_medis.py
+++ rekam_medis.py
@@ -37,12 +37,14 @@ def tambah_rekam_medis(data_pasien):
 
 
 def undo_tindakan(data_pasien):
+    # Validasi jika tidak ada riwayat tindakan yang tercatat di dalam stack
     if not riwayat_tindakan:
         print("Tidak ada tindakan yang bisa dibatalkan!")
         return
-
+    # LIFO: Mengambil (pop) ID pasien yang terakhir kali diberikan tindakan medis
     id_pasien = riwayat_tindakan.pop()
-
+    
+    # Menghapus catatan rekam medis terakhir yang ada di dalam list rekam_medis pasien tersebut
     if id_pasien in data_pasien and data_pasien[id_pasien].rekam_medis:
         data_pasien[id_pasien].rekam_medis.pop()
         print(f"Tindakan terakhir pasien {id_pasien} berhasil dibatalkan!")
